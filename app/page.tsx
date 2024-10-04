5cec5a764fd2927fade5f5b6bab364cb62843f93
sky_de_1991 <79264045+Sky-De@users.noreply.github.com>
2024-10-04 23:53:26 +0330

    file : assets add / constants add / types add / lib updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@ import { CollaborativeApp } from "./CollaborativeApp";
 import { Room } from "./Room";
 
 export default function Page() {
-  return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
-  );
+  return <h1 className="text-5xl text-white">LiveBlock - figma</h1>;
 }
+
+// /* <CollaborativeApp /> */
