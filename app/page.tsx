008cefe33f0dfba373c92e945dda47f0b130bd59
Mohammad <Mohammad.Aghajanpour@antriconsulting.com>
2024-10-08 18:33:53 +0330

    initialazatin: Live cursor component created / add ui and logic next step

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import { CollaborativeApp } from "./CollaborativeApp";
 import { Room } from "./Room";
 
 export default function Page() {
-  return <h1 className="text-5xl text-white">LiveBlock - figma</h1>;
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <h1 className="text-2xl text-white">LiveBlock - Figma Clone</h1>;
+    </div>
+  );
 }
 
 // /* <CollaborativeApp /> */
