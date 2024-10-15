db05efa2a409534fb018f4b264ccf16b23a3a05c
Mohammad <Mohammad.Aghajanpour@antriconsulting.com>
2024-10-15 23:36:39 +0330

    chore: Live updated to use CursorChat

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,9 +1,18 @@
-import LiveCursors from "./cursor/LiveCursors";
-import { useLiveHandlers } from "@/hooks/useLiveHandlers";
+import CursorChat from './cursor/CursorChat';
+import LiveCursors from './cursor/LiveCursors';
+import { useLiveHandlers } from '@/hooks/useLiveHandlers';
 
 const Live = () => {
-  const { others, handlePointerLeave, handlePointerMove, handlePointerDown } =
-    useLiveHandlers();
+  const {
+    others,
+    handlePointerLeave,
+    handlePointerMove,
+    handlePointerDown,
+    cursor,
+    cursorState,
+    setCursorState,
+    updateMyPresence,
+  } = useLiveHandlers();
 
   return (
     <div
@@ -13,6 +22,14 @@ const Live = () => {
       className="border-2 border-green-500 h-screen w-full flex justify-center items-center"
     >
       <h1 className="text-2xl text-white">LiveBlock - Figma Clone</h1>;
+      {cursor && (
+        <CursorChat
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        />
+      )}
       <LiveCursors others={others} />
     </div>
   );
