4caf1648a9773dc0992210570a1da3187a5f492e
Mohammad <Mohammad.Aghajanpour@antriconsulting.com>
2024-10-22 23:31:37 +0330

    BugFix: Fix cursor message box horizontal scroll on add to view

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -38,7 +38,7 @@ const CursorChat = ({
       {cursorState.mode === CursorMode.Chat && (
         <>
           <CursorSVG color="#000" />
-          <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
+          <div className="absolute right-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
             {cursorState.previousMessage && (
               <div>{cursorState.previousMessage}</div>
             )}
