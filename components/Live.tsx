c7bdc2b7473d665d88ddbe64dfae4d23f871b1fb
Mohammad <Mohammad.Aghajanpour@antriconsulting.com>
2024-10-29 14:20:57 +0330

    feat: Add user reaction and broadcasting it

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,54 @@
-import { CursorMode } from '@/types/type';
+import { CursorMode, ReactionEvent } from '@/types/type';
 import CursorChat from './cursor/CursorChat';
 import LiveCursors from './cursor/LiveCursors';
 import { useLiveHandlers } from '@/hooks/useLiveHandlers';
 import ReactionSelector from './reaction/ReactionBtn';
+import FlyingReaction from './reaction/FlyingReaction';
+import useInterval from '@/hooks/useInterval';
+import { useBroadcastEvent, useEventListener } from '@liveblocks/react';
 
 const Live = () => {
+  const broadcast = useBroadcastEvent();
+  useInterval(() => {
+    setReactions((reaction) =>
+      reaction.filter((r) => r.timestamp > Date.now() - 3000)
+    );
+  }, 1000);
+  useEventListener((eventData) => {
+    const event = eventData.event as ReactionEvent;
+    setReactions((preReaction) =>
+      preReaction.concat([
+        {
+          point: { x: event.x, y: event.y },
+          value: event.value,
+          timestamp: Date.now(),
+        },
+      ])
+    );
+  });
+  // move this
+  useInterval(() => {
+    if (
+      cursorState.mode === CursorMode.Reaction &&
+      cursorState.isPressed &&
+      cursor
+    ) {
+      setReactions((preReaction) =>
+        preReaction.concat([
+          {
+            point: { x: cursor.x, y: cursor.y },
+            value: cursorState.reaction,
+            timestamp: Date.now(),
+          },
+        ])
+      );
+    }
+    broadcast({
+      x: cursor?.x,
+      y: cursor?.y,
+      value: cursorState.reaction,
+    });
+  }, 200);
   const {
     others,
     handlePointerLeave,
@@ -29,6 +73,15 @@ const Live = () => {
       className="border-2 border-green-500 h-screen w-full flex justify-center items-center"
     >
       <h1 className="text-2xl text-white">LiveBlock - Figma Clone</h1>;
+      {reactions.map((r) => (
+        <FlyingReaction
+          key={r.timestamp.toString()}
+          x={r.point.x}
+          y={r.point.y}
+          timestamp={r.timestamp}
+          value={r.value}
+        />
+      ))}
       {cursor && (
         <CursorChat
           cursor={cursor}
