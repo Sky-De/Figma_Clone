b3aa4f7a2d2f5f0c3ff9207036626536542172f2
sky_de <sky_de1991@outlook.com>
2024-11-06 18:12:12 +0330

    fix: Update ActiveUsers.tsx + Update Avatar.tsx + Update generateRandomName

--- components/users/ActiveUsers.tsx
+++ components/users/ActiveUsers.tsx
@@ -2,6 +2,7 @@ import { useOthers, useSelf } from '@liveblocks/react';
 import { FC } from 'react';
 import { Avatar } from './Avatar';
 import styles from './ActiveUsers.module.css';
+import { generateRandomName } from '@/lib/utils';
 
 const ActiveUsers: FC = () => {
   const users = useOthers();
@@ -11,19 +12,21 @@ const ActiveUsers: FC = () => {
   return (
     <main className="flex h-screen w-full select-none place-content-center place-items-center">
       <div className="flex pl-3">
+        {currentUser && (
+          <Avatar name="You" otherStyles="border-[3px] border-primary-green" />
+        )}
+
         {users.slice(0, 3).map(({ connectionId, info }) => {
           return (
-            <Avatar key={connectionId} src={info.avatar} name={info.name} />
+            <Avatar
+              key={connectionId}
+              name={generateRandomName()}
+              otherStyles="-m;-3"
+            />
           );
         })}
 
         {hasMoreUsers && <div className={styles.more}>+{users.length - 3}</div>}
-
-        {currentUser && (
-          <div className="relative ml-8 first:ml-0">
-            <Avatar src={currentUser.info.avatar} name="You" />
-          </div>
-        )}
       </div>
     </main>
   );
