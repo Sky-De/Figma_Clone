eb282a35ad4b64e35bb2d911fd11ec01f5ac5d44
sky_de <sky_de1991@outlook.com>
2024-11-10 16:35:24 +0330

    feat: Done Avatar stack and other users

--- components/users/ActiveUsers.tsx
+++ components/users/ActiveUsers.tsx
@@ -1,5 +1,5 @@
 import { useOthers, useSelf } from '@liveblocks/react';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { Avatar } from './Avatar';
 import styles from './ActiveUsers.module.css';
 import { generateRandomName } from '@/lib/utils';
@@ -8,28 +8,36 @@ const ActiveUsers: FC = () => {
   const users = useOthers();
   const currentUser = useSelf();
   const hasMoreUsers = users.length > 3;
-
-  return (
-    <main className="flex h-screen w-full select-none place-content-center place-items-center">
-      <div className="flex pl-3">
-        {currentUser && (
-          <Avatar name="You" otherStyles="border-[3px] border-primary-green" />
-        )}
-
-        {users.slice(0, 3).map(({ connectionId, info }) => {
-          return (
+  const memoizedUsers = useMemo(() => {
+    return (
+      <div className="flex items-center justify-center gap-1 py-3">
+        <div className="flex pl-3">
+          {currentUser && (
             <Avatar
-              key={connectionId}
-              name={generateRandomName()}
-              otherStyles="-m;-3"
+              name="You"
+              otherStyles="border-[3px] border-primary-green"
             />
-          );
-        })}
+          )}
 
-        {hasMoreUsers && <div className={styles.more}>+{users.length - 3}</div>}
+          {users.slice(0, 3).map(({ connectionId }) => {
+            return (
+              <Avatar
+                key={connectionId}
+                name={generateRandomName()}
+                otherStyles="-m;-3"
+              />
+            );
+          })}
+
+          {hasMoreUsers && (
+            <div className={styles.more}>+{users.length - 3}</div>
+          )}
+        </div>
       </div>
-    </main>
-  );
+    );
+  }, [users.length]);
+
+  return memoizedUsers;
 };
 
 export default ActiveUsers;
