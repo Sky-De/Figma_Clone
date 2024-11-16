7ba93ed442c1ed4f24abc654513204916087e083
sky_de <sky_de1991@outlook.com>
2024-11-16 19:30:54 +0330

    feat: Add basic structure for left side bar

---
+++ components/RightSidebar.tsx
@@ -0,0 +1,9 @@
+const RightSidebar = () => {
+  return (
+    <section className="flex flex-col border-t border-primary-grey-300 min-w-[227px] sticky right-0 h-full max-sm:hidden select-none overflow-y-auto">
+      RightSidebar
+    </section>
+  );
+};
+
+export default RightSidebar;
