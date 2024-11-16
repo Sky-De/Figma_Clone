319533353cf8e3145b3e0aa516a270a06009675e
sky_de <sky_de1991@outlook.com>
2024-11-16 19:35:45 +0330

    feat: Add titles to sidebars + Add sidebars to layout

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,7 +1,7 @@
 const LeftSidebar = () => {
   return (
     <section className="flex flex-col border-t border-primary-grey-300 min-w-[227px] sticky left-0 h-full max-sm:hidden select-none overflow-y-auto">
-      LeftSidebar
+      <h3 className="px-5 pt-4 text-xs uppercase text-white">elements</h3>
     </section>
   );
 };
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,7 +1,7 @@
 const RightSidebar = () => {
   return (
     <section className="flex flex-col border-t border-primary-grey-300 min-w-[227px] sticky right-0 h-full max-sm:hidden select-none overflow-y-auto">
-      RightSidebar
+      <h3 className="px-5 pt-4 text-xs uppercase text-white">Design</h3>
     </section>
   );
 };
